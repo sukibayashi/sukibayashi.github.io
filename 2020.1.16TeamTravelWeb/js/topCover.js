082195fbb5001ce2550ce0782b8895a653342900
sukibayashi <tony9033bin@gmail.com>
2020-01-18 16:03:55 +0800

    update

---
+++ 2020.1.16TeamTravelWeb/js/topCover.js
@@ -0,0 +1,25 @@
+var layer1 = document.getElementById('layer1')
+scroll = window.pageYOffset;
+document.addEventListener('scroll', function (e) {
+    var offset = window.pageYOffset;
+    scroll = offset;
+    layer1.style.width = (50 + scroll / 5) + '%';
+    layer1.style.left = (-185 + scroll / 1) + 'px';
+})
+
+var layer2 = document.getElementById('layer2')
+scroll = window.pageYOffset;
+document.addEventListener('scroll', function (e) {
+    var offset = window.pageYOffset;
+    scroll = offset;
+    layer2.style.width = (50 + scroll / 5) + '%';
+    layer2.style.right = (-600 + scroll / 3) + 'px';
+})
+
+var text = document.getElementById('text')
+scroll = window.pageYOffset;
+document.addEventListener('scroll', function (e) {
+    var offset = window.pageYOffset;
+    scroll = offset;
+    text.style.top = -scroll / 10 + '%';
+})
